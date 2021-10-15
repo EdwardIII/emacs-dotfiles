8566fa358873273052d2a0c04537d5797fac8c1c
Edward Prendergast <edward@adaodesign.com>
2021-10-15 17:23:19 +0100

    Fix formatting

--- customisations/init-erc.el
+++ customisations/init-erc.el
@@ -14,14 +14,14 @@
 
 (defun credentials ()
   (nth 0 (auth-source-search :max 1
-			      :port erc-port ;; assumes you use port 1337 for your bnc
-			      :require '(:user :secret :port :host))))
+			     :port erc-port ;; assumes you use port 1337 for your bnc
+			     :require '(:user :secret :port :host))))
 
 ;; Just in case you need to print this out:
 ;; (setq eval-expression-print-level 9999)
 
 (set 'znc-servers
-      `((,(host (credentials)) ,erc-port t
-	 ((libera ,(user (credentials)) ,(password (credentials)))))))
+     `((,(host (credentials)) ,erc-port t
+	((libera ,(user (credentials)) ,(password (credentials)))))))
 
 
