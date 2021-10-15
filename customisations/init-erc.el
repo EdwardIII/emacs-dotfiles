5ba7ad228fbe5afcbbdf371aa08e46bd7bb79d10
Edward Prendergast <edward@adaodesign.com>
2021-10-15 18:01:03 +0100

    Refactor a bit

--- customisations/init-erc.el
+++ customisations/init-erc.el
@@ -1,27 +1,34 @@
-(require 'auth-source)
+;;; init-erc.el --- Pulls credentials from ~/.authinfo and sets up znc-servers  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021  Edward Prendergast
+
+;; Author: Edward Prendergast;;; init-erc.el --- Configure znc.el                 -*- lexical-binding: t; -*- <edward@EddBook.local>
+
+;;; Code:
+
+
+;; Copyright (C) 2021  Edward Prendergast
+
+;; Author: Edward Prendergast(require 'auth-source) <edward@EddBook.local>
+
 (require 'znc)
 
+;; assumes you use port 1337 for your bnc
 (setq-local erc-port "1337")
 
-(defun host (credentials)
-  (plist-get credentials :host))
-
-(defun user (credentials)
-  (plist-get credentials :user))
-
-(defun password (credentials)
+(defun decode-password (credentials)
   (funcall (plist-get credentials :secret)))
-
-(defun credentials ()
-  (nth 0 (auth-source-search :max 1
-			     :port erc-port ;; assumes you use port 1337 for your bnc
-			     :require '(:user :secret :port :host))))
 
 ;; Just in case you need to print this out:
 ;; (setq eval-expression-print-level 9999)
 
-(set 'znc-servers
-     `((,(host (credentials)) ,erc-port t
-	((libera ,(user (credentials)) ,(password (credentials)))))))
+(let ((credentials (nth 0 (auth-source-search :max 1
+					      :port erc-port
+					      :require '(:user :secret :port :host)))))
+  (set 'znc-servers
+       `((,(plist-get credentials :host) ,erc-port t
+  	  ((libera ,(plist-get credentials :user)
+  		   ,(decode-password credentials)))))))
 
-
+(provide 'init-erc)
+;;; init-erc.el ends here
